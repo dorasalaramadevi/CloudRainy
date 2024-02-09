301f81089b49c3a46f9dad14da713c476da08d3e
dorasalaramadevi <143689261+dorasalaramadevi@users.noreply.github.com>
2024-02-09 20:30:09 +0530

    Create Buildozer.spec.py

---
+++ exercise40/Buildozer.spec.py
@@ -0,0 +1,14 @@
+title = Hello World
+package.name = helloworld
+package.domain = org.example
+source.dir = .
+source.include_exts = py,png,jpg,kv,atlas
+source.include_patterns = assets/*,images/*.png
+version = 0.1
+requirements = kivy
+orientation = portrait
+fullscreen = 0
+android.permissions = INTERNET
+log_level = 2
+source.icon.filename = %(source.dir)s/data/icon.png
+orientation = portrait
